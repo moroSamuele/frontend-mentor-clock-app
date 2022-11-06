e39c44b39419f58334bd877e08da63c8a537b27e
Samuele Moro <moro96.samuele@icloud.com>
2022-11-06 13:39:48 +0100

    Programmazione dell'interfaccia grafica della Clock App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        halfBlack: "hsl(0, 0%, 19%)",
+      },
+      keyframes: {
+        up: {
+          "0%, 100%": { height: "10000px" },
+          "50%": { height: "0px" },
+        }
+      },
+      animation: {
+        up: "up 200ms ease-in-out"
+      },
+    },
   },
   plugins: [],
 }
